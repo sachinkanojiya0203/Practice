d89727010a92d9526deff3e465bc70105035e328
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-07-22 11:12:05 +0530

    ractice array

---
+++ js/10 array/index.js
@@ -0,0 +1,21 @@
+// Array
+
+const student=["Sachin","Dhaval","Bhavik"]
+
+console.log(student.length)  // check the lenght of the array
+console.log(student[0])
+console.log(student)
+
+
+student[0]="sunny"  // mutation
+console.log(student[0])
+console.log(student)
+
+// Push method
+
+student.push("rani")
+console.log(student)
+
+// pop method
+student.pop("Bhavik")
+console.log(student)
