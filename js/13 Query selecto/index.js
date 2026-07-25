9abb12b6e12dc9e941e7e0b782b5cadc4dd470f3
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-07-25 23:17:12 +0530

    LEARN qUERY SELECTOR

---
+++ js/13 Query selecto/index.js
@@ -0,0 +1,17 @@
+// Query selector (select the first element and change)
+
+/*
+selector
+1. class-.
+2. id- #
+3. tag name- <h1>
+
+*/
+
+const body=document.querySelector("body");
+const code=document.querySelector("code");
+code.innerHTML="<p>hello</p>" //use HTML
+
+const username=document.querySelector("#username")
+username.innerText="Dhaval"  //text
+// console.log(body)
