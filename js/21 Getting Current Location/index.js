65f2136ef06e94b6e1609a41219a8358aa827ba5
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-07-29 13:02:49 +0530

    getting current location using wheather api

--- js/21 Getting Current Location/index.js
+++ js/21 Getting Current Location/index.js
@@ -1,11 +1,17 @@
-const getbtn=document.getElementById('click-location')
-const ulocation=document.getElementById('USER-location')
+const getbtn = document.getElementById('click-location')
+const ulocation = document.getElementById('USER-location')
 
-getbtn.addEventListener('click',async ()=>{
-    const lvalue=navigator.geolocation.getCurrentPosition((position)=>{  //navigator 
-        console.log("get location",position)
-    },()=>{
-        console.log("there an issue")
+async function getdata(lat, long) {
+    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=11d9b92e44fb403091261754240208&q=${lat},${long}&aqi=yes`);
+    return await promise.json();
+}
+
+getbtn.addEventListener('click', async () => {
+    const lvalue = navigator.geolocation.getCurrentPosition(async (position) => {  //navigator 
+        const result = await getdata(position.coords.latitude, position.coords.longitude);
+        console.log("get location", result);
+    }, () => {
+        console.log("there an issue");
 
     });
-})
+});
