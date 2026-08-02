081775957901ab7913d101c5cb0ce8dca47a9559
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-08-02 12:56:28 +0530

    learn IIFE

---
+++ js/27 IIFE/index.js
@@ -0,0 +1,7 @@
+// IIFE (Immediately Invoked Function Expression) //time  6:09
+(function() {
+    console.log("IIFE executed!");
+})();
+
+// es6
+(()=>{console.log("IIFE executed with arrow function!");})();
