dc732c0f5186a6d6d89ef4facae6d60d728125c3
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-08-03 19:55:49 +0530

    make tic tac toe

--- js/30 tic tac toe game/index.js
+++ js/30 tic tac toe game/index.js
@@ -1,11 +1,35 @@
 let currentplayer="X";
-let array=Array(9).fill(null);
+let arr=Array(9).fill(null);
+
+function checkwinner(){
+    if(
+
+        (arr[0] !== null && arr[0]==arr[1]&&arr[1]==arr[2])||
+        (arr[3] !== null && arr[3]==arr[4]&&arr[4]==arr[5])||
+        (arr[6] !== null && arr[6]==arr[7]&&arr[7]==arr[8])||
+        (arr[0] !== null && arr[0]==arr[3]&&arr[3]==arr[6])||
+        (arr[1] !== null && arr[1]==arr[4]&&arr[4]==arr[7])||
+        (arr[2] !== null && arr[2]==arr[5]&&arr[5]==arr[8])||
+        (arr[0] !== null && arr[0]==arr[4]&&arr[4]==arr[8])||
+        (arr[2] !== null && arr[2]==arr[4]&&arr[4]==arr[6])
+    ){
+        document.write("winner is:",currentplayer)
+        return
+    }
+    if(!arr.some((e)=>e===null)){
+        document.write("Draw!")
+        return
+    }
+}
+
 
 function handleClick(el){
     const id=Number(el.id);
-    array[id]=currentplayer;
+    if(arr[id]!==null)return;
+    arr[id]=currentplayer;
     el.innerText=currentplayer
+    checkwinner();
     currentplayer=currentplayer  === "X" ? "O" : 'X'
-    console.log(array);
+    // console.log(array);
 
 }
