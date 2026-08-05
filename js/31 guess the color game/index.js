764743c33557962e833af52b1becd6053c4271be
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-08-05 23:12:55 +0530

    minor changes in color game

--- js/31 guess the color game/index.js
+++ js/31 guess the color game/index.js
@@ -1,6 +1,5 @@
 const colorcodecontainer=document.getElementById("color-code");
 const optionscontainer=document.getElementById("option-container")
-const scorecontainer=document.getElementById("score")
 let randomcolor=null;
 let score=0;
 function generaterandomNumberBetween(min,max){
@@ -16,36 +15,27 @@ function generaterandomColorRGB(){
 
 }
 function incrementscore(){
-    score++;
-    scorecontainer.innerText=score;
-     localStorage.setItem("score", score);
+    score += 1;
+    document.getElementById('score').innerText=score;
 }
 // console.log(generaterandomColorRGB());
 function validateresult(el){
     // console.log(el.target)
-    
     const selectcolor=el.target.style.backgroundColor ;
     if(selectcolor === randomcolor){
-        incrementscore();
+        incrementscore()
     }else{
-        score=0;
-        scorecontainer.innerText=score;
-        localStorage.setItem("score",score)
+        score=0
     }
     startgame();
-     
 };
 
 function startgame(){
-    /*
-    score=Number(window.localStorage.getItem('score'))||0;
-    scorecontainer.innerText=score*/
-
-    optionscontainer.innerHTML="";
+    optionscontainer.innerHTML=null;
     randomcolor=generaterandomColorRGB();
     colorcodecontainer.innerText=randomcolor;
 
-    const ansindex=generaterandomNumberBetween(0, 5);
+    const ansindex=generaterandomNumberBetween(0,5);
     
     for(let i=0; i<6; i++){
         const div=document.createElement("div");
@@ -55,9 +45,4 @@ function startgame(){
     }
 }
 
-// window.addEventListener('load',()=>startgame())
-window.addEventListener("load", () => {
-    score = Number(localStorage.getItem("score")) || 0;
-    scorecontainer.innerText = score;
-    startgame();
-});
+window.addEventListener('load',()=>startgame())
