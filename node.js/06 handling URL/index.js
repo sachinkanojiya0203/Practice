f8d0e88669276919c859dee9ede4055b81e348d7
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-08-09 22:51:17 +0530

    make a query parameter in url

---
+++ node.js/06 handling URL/index.js
@@ -0,0 +1,23 @@
+const http=require('http');
+const fs=require('fs');
+const url=require('url')
+const port=8000;
+
+const myserver=http.createServer((req,res)=>{
+    if(req.url ==='/favicon.ico')return res.end();
+    const log= `${Date.now()}:${req.url} :new request received \n`
+    const myurl=url.parse(req.url,true);    // query parameter in url 
+    console.log(myurl);
+    fs.appendFile('log.txt',log,(err,data)=>{
+        switch(myurl.pathname){
+            case'/': res.end("HOME");
+            break;
+            case'/about':res.end("ABOUT");
+            break
+            default:res.end("404 not Found!");
+
+        }
+    })
+})
+
+myserver.listen(port,()=>console.log(`server started port no : ${port}`))
