d5043b567f8341736db04f34fda951e86c09e2df
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-08-14 16:22:50 +0530

    working with method in url

--- node.js/10 Rest API/index.js
+++ node.js/10 Rest API/index.js
@@ -1,35 +1,49 @@
-const http=require('http');
-const express=require('express')
-const users=require('./user.json')
-const port=8000;
+const http = require('http');
+const express = require('express')
+const users = require('./user.json')
+const port = 8000;
 
-const app=express();
+const app = express();
 
 
 // ROUTES
 
-app.get('/users',(req,res)=>{
-    const html=`
+app.get('/users', (req, res) => {
+    const html = `
     <ul>
-    ${users.map((users)=>`<li> ${users.first_name}</li>`).join("    ")}
+    ${users.map((users) => `<li> ${users.first_name}</li>`).join("    ")}
     </ul>
     `
     res.send(html)
- })
- 
- // Rest api
+})
 
- app.get('/api/users',(req,res)=>{
-     return res.json(users);
-    });
+// Rest api
+
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+});
 
 //URL using id
 
-app.get('/api/users/:id',(req,res)=>{
-    const id=Number(req.params.id);
-    const user=users.find(((users)=>users.id===id));
-    return res.json(user)
+
+app.post('/api/users', (req, res) => {
+    // TODO: create new user
+    return res.json({ status: "pending" });
 })
 
+app.route('/api/users/:id').get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(((users) => users.id === id));
+    return res.json(user)
+}).patch((req, res) => {
+    // TODO: edit user with id
+    return res.json({ status: "pending" });
+}).delete((req, res) => {
+    // TODO: edit user with id
+    return res.json({ status: "pending" });
+});
 
-app.listen(port,()=>console.log(`server started! ${port}`))
+
+
+
+app.listen(port, () => console.log(`server started! ${port}`))
