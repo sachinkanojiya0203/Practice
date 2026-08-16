5b04992030c5cd44fc42442309ab0c0dc656b726
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-08-16 23:32:15 +0530

    minor changes

--- node.js/10 Rest API/index.js
+++ node.js/10 Rest API/index.js
@@ -47,3 +47,52 @@ app.route('/api/users/:id').get((req, res) => {
 
 
 app.listen(port, () => console.log(`server started! ${port}`))
+const http = require('http');
+const express = require('express')
+const users = require('./user.json')
+const port = 8000;
+
+const app = express();
+
+
+// ROUTES
+
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+    ${users.map((users) => `<li> ${users.first_name}</li>`).join("    ")}
+    </ul>
+    `
+    res.send(html)
+})
+
+// Rest api
+
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+});
+
+//URL using id
+
+
+app.post('/api/users', (req, res) => {
+    // TODO: create new user
+    return res.json({ status: "pending" });
+})
+
+app.route('/api/users/:id').get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(((users) => users.id === id));
+    return res.json(user)
+}).patch((req, res) => {
+    // TODO: edit user with id
+    return res.json({ status: "pending" });
+}).delete((req, res) => {
+    // TODO: edit user with id
+    return res.json({ status: "pending" });
+});
+
+
+
+
+app.listen(port, () => console.log(`server started! ${port}`))
