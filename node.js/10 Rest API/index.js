4253e1807899e72cc8751310aa5fc4e505ef77c4
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-08-17 14:55:43 +0530

    minor changes

--- node.js/10 Rest API/index.js
+++ node.js/10 Rest API/index.js
@@ -1,11 +1,13 @@
 const http = require('http');
+const fs=require('fs')
 const express = require('express')
 const users = require('./user.json')
 const port = 8000;
 
 const app = express();
 
-
+// using middleware - plugin
+app.use(express.urlencoded({extended:false}))
 // ROUTES
 
 app.get('/users', (req, res) => {
@@ -27,57 +29,13 @@ app.get('/api/users', (req, res) => {
 
 
 app.post('/api/users', (req, res) => {
-    // TODO: create new user
-    return res.json({ status: "pending" });
-})
+    const body=req.body
+    users.push({...body,id:users.length+1});
+    fs.writeFile("./user.json" ,JSON.stringfy(users),(err,data)=>{
 
-app.route('/api/users/:id').get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find(((users) => users.id === id));
-    return res.json(user)
-}).patch((req, res) => {
-    // TODO: edit user with id
-    return res.json({ status: "pending" });
-}).delete((req, res) => {
-    // TODO: edit user with id
-    return res.json({ status: "pending" });
-});
-
-
-
-
-app.listen(port, () => console.log(`server started! ${port}`))
-const http = require('http');
-const express = require('express')
-const users = require('./user.json')
-const port = 8000;
-
-const app = express();
-
-
-// ROUTES
-
-app.get('/users', (req, res) => {
-    const html = `
-    <ul>
-    ${users.map((users) => `<li> ${users.first_name}</li>`).join("    ")}
-    </ul>
-    `
-    res.send(html)
-})
-
-// Rest api
-
-app.get('/api/users', (req, res) => {
-    return res.json(users);
-});
-
-//URL using id
-
-
-app.post('/api/users', (req, res) => {
-    // TODO: create new user
-    return res.json({ status: "pending" });
+        // TODO: create new user
+        return res.json({ status: "success",id:users.length+1 });
+    })
 })
 
 app.route('/api/users/:id').get((req, res) => {
