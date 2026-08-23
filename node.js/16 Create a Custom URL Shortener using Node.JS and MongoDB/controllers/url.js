8279e39eb75d95e4c27d20a4c8752c5cbdd6d580
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-08-23 22:08:19 +0530

    minor changes

--- node.js/16 Create a Custom URL Shortener using Node.JS and MongoDB/controllers/url.js
+++ node.js/16 Create a Custom URL Shortener using Node.JS and MongoDB/controllers/url.js
@@ -1,10 +1,10 @@
-const {nanoid}=require('nanoid')
+const shortid=require('shortid')
 const URL = require('../models/url');
 
 async function HandlegenerateNewURL(req,res) {
     const body=req.body;
     if(!body.url)return res.status(400).json({error:"Url Required"});
-    const ShortID = nanoid(8);
+    const ShortID = shortid();
     await URL.create({
         shortid:ShortID,
         redirectURL:body.url,
--- node.js/16 Create a Custom URL Shortener using Node.JS and MongoDB/index.js
+++ node.js/16 Create a Custom URL Shortener using Node.JS and MongoDB/index.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const URLroutes=require('./routes/url')
+const {connectTomongoDB}=require('./connect')
 const app=express();
 const port=8001;
 
 
-
-
+connectTomongoDB('mongodb://127.0.0.1:27017/Short-url').then(()=>console.log("MOngoDB Connected!"))
+app.use(express.json());    
 
 
 app.use('/url',URLroutes);
