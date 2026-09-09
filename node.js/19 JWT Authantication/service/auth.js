d11f7476478aea0de192188c4f7468f71e81fc2e
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-09-09 23:41:57 +0530

    minor changes

--- node.js/19 JWT Authantication/service/auth.js
+++ node.js/19 JWT Authantication/service/auth.js
@@ -3,11 +3,21 @@ const secret = 'sachin@7885$'
 
 
 function setUSer(User) {
-    return jwt.sign(payload, secret);
+
+    return jwt.sign({
+        _id:User._id,
+        email:User.email,
+    },secret);
 }
 
 function getUSer(token) {
-    return jwt.verify(token,secret);
+    if(!token)return null;
+
+    try {
+        return jwt.verify(token,secret);
+    } catch (error) {
+        return null;
+    }
 }
 
 
