e5e354fcb628cb71e7f34baae02fa3f238cff680
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-09-16 00:18:28 +0530

    sudden changes

--- node.js/23 Uploading Files with NodeJS and Multer/index.js
+++ node.js/23 Uploading Files with NodeJS and Multer/index.js
@@ -1,18 +1,35 @@
 const path=require("path")
 const express=require("express")
-
+const multer=require("multer")
 
 const app=express();
 const PORT=8000;
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        return cb(null,"/uploads")
+    },
+    filename:function(req,file,cb){
+        return cb(null,`${Date.now()}-${file.originalname}`);
+    },
+});
 
+const upload=multer({storage})
 app.set('view engine', 'ejs'); 
 app.set("View",path.resolve("./views"));
 
-app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.get("/",(req,res)=>{
     return res.render("homepage")
 });
 
+app.post("/upload",upload.single('ProfileImage'),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
 
-app.listen(PORT,()=>console.log(`server started Port No: http://localhost:${PORT}`))
+    return res.redirect("/")
+
+})
+
+
+app.listen(PORT,()=>console.log(`server started Port No: http://localhost:${PORT}`))                    
