311103c7bfaafd895e39de37a49b556b938e55fd
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-09-18 17:04:59 +0530

    add blog page

--- node.js/25 Setting up Authentication - NodeJS Blogging Application with MongoDB/index.js
+++ node.js/25 Setting up Authentication - NodeJS Blogging Application with MongoDB/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path=require("path")
+const cookieParser=require('cookie-parser')
 const app = express();
 const mongoose=require("mongoose")
 const port = 8000
 const Userroutes=require("./routes/user.js");
+const Blogroutes=require("./routes/blog.js");
+const Blog=require('./models/blog.js')
 const { connected } = require('process');
+const { checkForAuthenticationCookie } = require('./middlewares/authantication.js');
 
 mongoose.connect("mongodb://localhost:27017/BlogTech").then((e)=>console.log("mongoDB connected"));
 
@@ -12,9 +16,16 @@ app.set("view engine","ejs")
 app.set("views",path.resolve("./views"));
 
 app.use(express.urlencoded({extended:false}));
-app.get("/",(req,res)=>{
-    res.render("home")
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"))
+app.get("/",async(req,res)=>{
+    const allBlogs=await Blog.find({});
+    res.render("home",{
+        user:req.user,
+        blogs:allBlogs,
+    })
 });
-app.use("/user",Userroutes)
+app.use("/user",Userroutes);
+app.use("/blog",Blogroutes);
 
 app.listen(port, () => console.log(`server Started port:  http://localhost:${port}`))
---
+++ node.js/25 Setting up Authentication - NodeJS Blogging Application with MongoDB/routes/blog.js
@@ -0,0 +1,42 @@
+const{Router}=require('express');
+const multer=require("multer");
+const path=require("path")
+const Blog=require("../models/blog")
+
+const router=Router();
+
+const crypto = require('crypto')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`))
+  },
+  filename: function (req, file, cb) {
+    const filename=`${Date.now()}-${file.originalname}`
+    cb(null,filename);
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
+router.get("/add-new",(req,res)=>{
+    return res.render('addblog',{
+        user:req.user
+    });
+
+})
+router.post("/",upload.single("coverImage"),async(req,res)=>{
+    const {title,body}=req.body
+    const blog =await Blog.create({
+        body,
+        title,
+        createdBy:req.user._id,
+        coverImageURL:`/uploads/${req.file.filename}`
+    })
+    return res.redirect(`/blog/${blog._id}`);
+
+})
+
+
+module.exports=router;
