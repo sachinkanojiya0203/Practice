b7c51fb0d6cd9ad7ccf037d971d10aade4ff2f0e
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-09-20 23:38:44 +0530

    minor changes

--- node.js/25 Setting up Authentication - NodeJS Blogging Application with MongoDB/routes/blog.js
+++ node.js/25 Setting up Authentication - NodeJS Blogging Application with MongoDB/routes/blog.js
@@ -26,6 +26,15 @@ router.get("/add-new",(req,res)=>{
     });
 
 })
+router.get("/:id",async(req,res)=>{
+  const blog= await Blog.findById(req.params.id).populate("createdBy");
+  return res.render("blog",{
+    user:req.user,
+    blog
+  })
+});
+
+
 router.post("/",upload.single("coverImage"),async(req,res)=>{
     const {title,body}=req.body
     const blog =await Blog.create({
