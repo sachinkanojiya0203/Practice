e59fcabbb374fd6b5569c9b417a4b201cd38ede2
Sachin kanojiya <111424858+sachinkanojiya0203@users.noreply.github.com>
2026-09-22 23:27:08 +0530

    add comment section

--- node.js/25 Setting up Authentication - NodeJS Blogging Application with MongoDB/routes/blog.js
+++ node.js/25 Setting up Authentication - NodeJS Blogging Application with MongoDB/routes/blog.js
@@ -29,19 +29,22 @@ router.get("/add-new", (req, res) => {
 })
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments= await Comment.find({ blogId:req.params.id}).populate("createdBy")
+  console.log("comments",comments)
   return res.render("blog", {
     user: req.user,
-    blog
+    blog,
+    comments,
   })
 });
 
-router.post("/comment/:blodId", async (req, res) => {
+router.post("/comment/:blogId", async (req, res) => {
   await Comment.create({
-    content: res.body.content,
+    content: req.body.content,
     blogId: req.params.blogId,
     createdBy: req.user._id,
   });
-  return res.redirect(`/blog/${req.params.blodId}`)
+  return res.redirect(`/blog/${req.params.blogId}`)
 })
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
